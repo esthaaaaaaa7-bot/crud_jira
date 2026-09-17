beaff234ebd8f5f1311228efc6018589f1fa9c3c
daun25555 <adra8b@gmail.com>
2026-09-17 09:36:31 +0700

    pengaturan

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -101,11 +101,12 @@
             </li>
 
             <li>
-                <a href="#" class="flex items-center gap-3 py-2 px-4 rounded-lg text-gray-400 hover:bg-[#1a1a1a] hover:text-white transition duration-200 mt-2">
+                <a href="{{ url('/setting') }}" class="flex items-center gap-3 py-2 px-4 rounded-lg text-gray-400 hover:bg-[#1a1a1a] hover:text-white transition duration-200 mt-2 cursor-pointer">
                     <i class="fa-solid fa-gear text-sm w-5 text-center"></i>
                     <span class="text-sm">Settings</span>
                 </a>
             </li>
+
 
         </ul>
 
